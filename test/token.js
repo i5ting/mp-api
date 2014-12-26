f6c22701796f1a1adba4ad974149b665fe9433ba
alfred sang <i5ting@126.com>
2014-12-26 13:29:37 +0800

    for test

--- test/token.js
+++ test/token.js
@@ -5,11 +5,34 @@ var request = require('supertest')
 var app = require('../app')
 
 describe('GET /api/token', function(){
-  it('respond with json', function(done){
+  it('respond with ok', function(done){
     request(app)
       .get('/api/token?grant_type=client_credential&appid=APPID&secret=APPSECRET')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200,{"access_token":"ACCESS_TOKEN","expires_in":7200}, done);
   })
+	
+  it('respond with error', function(done){
+    request(app)
+      .get('/api/token?grant_type=client_credential2&appid=APPID&secret=APPSECRET')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200,{
+				"errcode":40014,
+				"errmsg":"invalid grant_type"
+			}, done);
+  })
+	
+  it('respond with error2', function(done){
+    request(app)
+      .get('/api/token?grant_type=client_credential')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200,{
+				"errcode":40013,
+				"errmsg":"invalid appid"
+			}, done);
+  })
+	
 })
